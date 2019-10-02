744be2ae54d57c0006e9c6b027b459984a714a9a
Trevor Jove Cruz <cruztr@oocl.com>
2019-10-02 16:29:00 +0800

    do jquery practice exercise

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,8 @@
 $(document)
     .ready(function () {
 
+        let itemList = [];
+
         function generateUUID() {
             /*jshint bitwise:false */
             var i,
@@ -21,5 +23,87 @@ $(document)
             return uuid;
         }
 
-        // code to be implemented
+        function addItem() {
+            let input = $('input[name = ListItem]').val();
+
+            let itemObj = {
+                id: generateUUID(),
+                name: input
+            };
+
+            itemList.push(itemObj);
+
+            renderToDoList();
+
+            $('input[name=ListItem]').val('');
+        }
+
+        function renderToDoList() {
+            $('#list').empty();
+
+            itemList.forEach(function(item) {
+                let todoList = document.createElement('li');
+                todoList.id = item.id;
+                todoList.class = "";
+
+                todoList.innerHTML = '<input name = "done-todo" type="checkbox" class="done-todo" /><span>' + item.name + '</span>';
+
+                document.getElementById("list").appendChild(todoList);
+            });
+        }
+
+        $('#button').click(addItem);
+
+        $("input[name=ListItem]").keyup(function (event) {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                addItem();
+            }
+        });
+
+        $(document).on('click', 'input[name=done-todo]', function (event) {
+            $(this).parent()
+                .toggleClass('checked');
+        });
+
+        $('#filters li a').on('click', function(){
+            var checked = $("li[class='checked']");
+            var unchecked = $("li[class='']");
+
+            if (this.dataset.filter === "all") {
+                checked.show();
+                unchecked.show();
+            } else if (this.dataset.filter === "active") {
+                checked.hide();
+                unchecked.show();
+            } else if (this.dataset.filter === "complete") {
+                checked.show();
+                unchecked.hide();
+            }
+        });
+
+        $(document).on('dblclick', 'li', function () {
+            $(this)
+                .children('span')
+                .attr('contentEditable', 'true')
+                .focus()
+                .keypress(function (event) {
+                    var keycode = (event.keyCode
+                        ? event.keyCode
+                        : event.which);
+                    if (keycode === '13') {
+                        event.target.blur();
+
+                        $(this).children('span')
+                            .attr('contenteditable', 'false');
+
+                        todoList.find(element => element.id === $(this).parent()[0].id)
+                            .name = $(this).text();
+
+                        renderTodoList();
+                    }
+                });
+        });
+
+
     });
